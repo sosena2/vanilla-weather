36ec28a364544d5e3e00ab466ea9b54b006e28d6
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-08-23 16:55:07 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
 function updateWeather(response){
+    console.log(response);
     let temperatureValue=document.querySelector("#temp-value");
     let temperature=Math.round(response.data.temperature.current);
     temperatureValue.innerHTML=temperature;
+
     let cityElement=document.querySelector("#city");
     cityElement.innerHTML=response.data.city;
+
+    let descriptionElement=document.querySelector("#weather-description");
+    descriptionElement.innerHTML=response.data.condition.description;
+
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+
+    let windSpeedElement=document.querySelector("#wind-speed");
+    windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+
+    let timeElement = document.querySelector("#time");
+    
+    let date = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
+    
+
 }
+
+function formatDate(date){
+let minutes=date.getMinutes();
+let hours=date.getHours();
+let days=[ "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function citysearch(city){
 let apiKey="ao271fb34td73aa51030f96bdd214af0";
 apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(updateWeather);
 }
+
 function showSearchInput(event){
 event.preventDefault();
 let searchInput=document.querySelector("#form-input");
@@ -17,6 +56,7 @@ let searchInput=document.querySelector("#form-input");
 //cityElement.innerHTML=searchInput.value;
 citysearch(searchInput.value);
 }
+
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",showSearchInput);
-citysearch();
+citysearch("paris");
