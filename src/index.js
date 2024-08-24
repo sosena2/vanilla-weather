cffe8b6f01dbbf7c78b5d60dd97ef2d62ae0391c
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-08-24 15:50:57 +0300

    injected html from js

--- src/index.js
+++ src/index.js
@@ -57,7 +57,27 @@ let searchInput=document.querySelector("#form-input");
 //cityElement.innerHTML=searchInput.value;
 citysearch(searchInput.value);
 }
+function displayForecast(){
+  //injecting css from js
+  let days=["Tue","Wed","Thu","Fri","Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day){
+    forecastHtml =
+    forecastHtml +
+  `<div class="weather-forecast-date">
+  <div class="weather-forecast-day">${day}</div>
+  <div class="weather-forecast-icon">☀️</div>
+  <div class="weather-forecast-values">
+    <div class="weather-forecast-value"><strong>15&deg</strong></div>
+    <div class="weather-forecast-value">14&deg</div>
+  </div>
+  </div>`;
+  });
+  let forecastElement=document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",showSearchInput);
 citysearch("paris");
+displayForecast();
